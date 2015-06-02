3dfe1e8389c335faaa7a97bb822c0180e3f868ff
DalikarFT <ian.bouldin@gmail.com>
2015-06-01 20:50:51 -0700

    Made a few changes

--- more.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'ian'
-class More():
-    def apple(self):
-       print("I AM APPLES!")
-    tangerine = "Living reflection of a dream"
-    myStuff = {'apple': "its dictionary magic"}
-
-
